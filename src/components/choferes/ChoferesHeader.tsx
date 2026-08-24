f5916b955795294a20f6f2714b81f58f0d706d93
iker20 <ikervegavaldes123@gmail.com>
2026-08-24 14:52:09 -0400

    Hacer funcional el botón Nuevo Chofer

--- src/components/choferes/ChoferesHeader.tsx
+++ src/components/choferes/ChoferesHeader.tsx
@@ -1,8 +1,12 @@
 // src/components/choferes/ChoferesHeader.tsx
 
-function ChoferesHeader() {
+type ChoferesHeaderProps = {
+  onNuevoChofer: () => void;
+};
+
+function ChoferesHeader({ onNuevoChofer }: ChoferesHeaderProps) {
   return (
-    <header className="dashboard-header">
+    <header className="dashboard-header choferes-header">
       <div>
         <h1>Choferes</h1>
         <p>Gestión y administración de choferes</p>
@@ -11,6 +15,7 @@ function ChoferesHeader() {
       <button
         className="add-driver-button"
         type="button"
+        onClick={onNuevoChofer}
       >
         + Nuevo Chofer
       </button>
